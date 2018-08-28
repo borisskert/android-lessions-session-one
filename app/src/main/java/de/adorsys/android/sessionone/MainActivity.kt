bbd4a61d31e573c77ceee92bc39199994329e38a
Boris Skert <boris.skert@adorsys.de>
2018-08-28 18:37:46 +0200

    change text dynamically

--- app/src/main/java/de/adorsys/android/sessionone/MainActivity.kt
+++ app/src/main/java/de/adorsys/android/sessionone/MainActivity.kt
@@ -2,11 +2,19 @@ package de.adorsys.android.sessionone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.myTextView)
+        textView.text = "irgendwas neues 🙈"
+
+        val button = findViewById<Button>(R.id.myButton)
+        button.text = "Click me 🐶"
     }
 }
